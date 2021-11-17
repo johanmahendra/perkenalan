2aca9262bb7428b3a694e79b646f3aac1448196f
johanmahendra <94509033+johanmahendra@users.noreply.github.com>
2021-11-17 11:40:11 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,7 @@
+nama = 'Johan Mahendra Syah Putra'
+nim = 212410102075
+prodi = 'Teknologi Informasi'
+univ = 'Universitas Jember'
+asal = 'Banyuwangi'
+
+print('Halo perkenalkan nama saya', nama,'\ndengan NIM', nim, '\ndari prodi', prodi, univ, '\ndan saya berasal dari', asal)
